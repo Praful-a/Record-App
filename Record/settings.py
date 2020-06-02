877e499a881a634b9b1ca2f6a65a248ed41cf698
Praful-a <prafuldubey27@gmail.com>
2020-06-02 18:18:56 +0530

    added home page.

--- Record/settings.py
+++ Record/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "home"),
+    os.path.join(BASE_DIR, "Home"),
     os.path.join(BASE_DIR, "Register"),
     os.path.join(BASE_DIR, "Login"),
     os.path.join(BASE_DIR, "Form")
